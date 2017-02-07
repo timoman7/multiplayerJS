6ebdf42e1dd89c3a3f2575af62fffacca1fadd20
timoman7 <tc20791@sestudent.org>
2017-02-07 15:36:11 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,24 @@
+var currentUser;
+var users;
+firebase.auth().getRedirectResult().then(function(result){
+		var user = result.user;
+		var credential = result.credential;
+		if(user===null){
+			var provider;
+			provider = new firebase.auth.GoogleAuthProvider();
+			firebase.auth().signInWithRedirect(provider);
+		}else{
+			currentUser = firebase.auth().currentUser;
+		}
+	},function(error) {
+    var email = error.email;
+    var credential = error.credential;
+});
+firebase.database().ref('/users').once('value').then(
+  function(snapshot){
+    users = snapshot.val();
+  }
+);
 function setup() {
   
 }
