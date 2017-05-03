67efea39273e18372a621c94cca2d56e90fe4252
timoman7 <tc20791@sestudent.org>
2017-05-03 14:19:48 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1077,7 +1077,7 @@ function Entity(x,y,radius,name,maxHP,bullet,isPlayer,mainPlayer){
 			this.bullet=allBullets[data.bulletName];
 			this.canJump=data.canJump;
 			this.colliding=data.colliding;
-			this.collisionPointY=data.collisionPointX || 0;
+			this.collisionPointX=data.collisionPointX || 0;
 			this.collisionPointY=data.collisionPointY || 0;
 			this.controls=data.controls;
 			this.crosshair = createVector(data.crosshair.x,data.crosshair.y,data.crosshair.z);
