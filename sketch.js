1e5a8a9c21aec1889d4301086707564b7c12eb74
timoman7 <tc20791@sestudent.org>
2017-05-03 12:45:27 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -37,7 +37,7 @@ function signOut(){
 		alert("Somehow you screwed up logging out.");
 	});
 }
-/*
+
 var globals, keys, mouse, buttons, platforms, f, fp, fps, framerate, frames, bullet_sound, bullet_hit, rocket_sound, rocket_explode, state, dbug, gravity, bg, player, test, testPlat, testPlat2, Tau, enemyBullet, minigunBullet, defaultBullet, rocketBullet, p1c, setBG, backToMenu, backToMenu2, backToMenu3, playGame, helpBtn, testDrop, bgR, bgG, bgB, bgrP1Btn, bgrP5Btn, bgrP10Btn, bgrM1Btn, bgrM5Btn, bgrM10Btn, bggP1Btn, bggP5Btn, bggP10Btn, bggM1Btn, bggM5Btn, bggM10Btn, bgbP1Btn, bgbP5Btn, bgbP10Btn, bgbM1Btn, bgbM5Btn, bgbM10Btn;
 //Sin angle / hyp = Y
 //Cos angle / hyp = X
@@ -1237,7 +1237,7 @@ function mouseReleased(){
     }
 }
 //Mouse & Key Events
-
+/*
 function setup(){
   createCanvas(600,600);
   colorMode(RGB);
